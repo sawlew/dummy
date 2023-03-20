2dbd7f17556a78090834f1f35abdffa5de035c8a
Soliu Adeosun <sawlew@gmail.com>
2023-03-20 03:27:41 +0100

    more codes

--- index.js
+++ index.js
@@ -21,3 +21,28 @@ function seemore(){
         y.style.display = "flex";
     }
 }
+
+function warning(){
+    var b = document.getElementById("warning_div");
+    if (b.style.display === "none") {
+        b.style.display = "block";
+    }else {
+        b.style.display = "none";
+    }
+}
+
+function offline(){
+    var offline = document.getElementById("offline");
+    if (offline.style.display === "flex"){
+        offline.style.display = "none";
+    }
+}
+
+function chat(){
+    var m = document.getElementById("message");
+    if (m.style.display === "none"){
+        m.style.display = "block";
+    }else{
+        m.style.display = "none";
+    }
+}
